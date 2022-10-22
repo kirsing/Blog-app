20d595389548d07dbb4d543f0ea021f773f443fb
kirsing <kirsing98@icloud.com>
2022-10-22 14:42:27 +0300

    initial commit

---
+++ src/test/java/com/example/myblogapplication/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
