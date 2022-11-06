c9d33a315ee8c3d74d680b6e449aec9cb0cdd1de
kirsing <kirsing98@icloud.com>
2022-11-06 12:31:20 +0300

    added JWT, fixed and improved authentication with selection between admin or user by default

--- src/main/java/com/example/myblogapplication/controller/AuthController.java
+++ src/main/java/com/example/myblogapplication/controller/AuthController.java
@@ -2,10 +2,12 @@ package com.example.myblogapplication.controller;
 
 import com.example.myblogapplication.model.Role;
 import com.example.myblogapplication.model.User;
+import com.example.myblogapplication.payload.JWTAuthResponse;
 import com.example.myblogapplication.payload.LoginDTO;
 import com.example.myblogapplication.payload.SignUpDTO;
 import com.example.myblogapplication.repository.RoleRepository;
 import com.example.myblogapplication.repository.UserRepository;
+import com.example.myblogapplication.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.ws.Response;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -38,12 +42,20 @@ public class AuthController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+
     @PostMapping("/signin")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<JWTAuthResponse> authenticateUser(@RequestBody LoginDTO loginDTO) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginDTO.getUsernameOrEmail(), loginDTO.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("User signed in successfully", HttpStatus.OK);
+
+        // get token from tokenProvider
+        String token = tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTAuthResponse(token));
     }
 
     @PostMapping("/signup")
@@ -59,9 +71,24 @@ public class AuthController {
         user.setEmail(signUpDTO.getEmail());
         user.setPassword(passwordEncoder.encode(signUpDTO.getPassword()));
 
-        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
-        user.setRoles(Collections.singleton(roles));
+        Set<String> strRoles = signUpDTO.getRole();
+        Set<Role> roles = new HashSet<>();
 
+        strRoles.forEach(role -> {
+            switch(role) {
+                case "admin":
+                    Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
+                    roles.add(adminRole);
+                    break;
+
+                default:
+                    Role userRole = roleRepository.findByName("ROLE_USER").get();
+                    roles.add(userRole);
+            }
+        });
+//        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+//        user.setRoles(Collections.singleton(roles));
+        user.setRoles(roles);
         userRepository.save(user);
         return new ResponseEntity<>("User registered", HttpStatus.OK);
     }
--- src/main/java/com/example/myblogapplication/payload/SignUpDTO.java
+++ src/main/java/com/example/myblogapplication/payload/SignUpDTO.java
@@ -2,10 +2,13 @@ package com.example.myblogapplication.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class SignUpDTO {
     private String name;
     private String username;
     private String email;
     private String password;
+    private Set<String> role;
 }
