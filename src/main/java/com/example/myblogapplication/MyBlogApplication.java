ec1e4dab2ef471992acde9d3ea1b314b85bf6c57
kirsing <kirsing98@icloud.com>
2022-11-13 12:33:04 +0300

    configure cloud service

--- src/main/java/com/example/myblogapplication/MyBlogApplication.java
+++ src/main/java/com/example/myblogapplication/MyBlogApplication.java
@@ -1,12 +1,15 @@
 package com.example.myblogapplication;
 
+
+import com.example.myblogapplication.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MyBlogApplication {
+public class MyBlogApplication  {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
@@ -15,5 +18,8 @@ public class MyBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyBlogApplication.class, args);
     }
+
+    @Autowired
+    private RoleRepository roleRepository;
 
 }
