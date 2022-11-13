aea3e8f5e8686f4a01885c097c00c832842d15a9
kirsing <kirsing98@icloud.com>
2022-11-13 15:21:01 +0300

    some fixed in main class

--- src/main/java/com/example/myblogapplication/MyBlogApplication.java
+++ src/main/java/com/example/myblogapplication/MyBlogApplication.java
@@ -1,15 +1,17 @@
 package com.example.myblogapplication;
 
 
+import com.example.myblogapplication.model.Role;
 import com.example.myblogapplication.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MyBlogApplication  {
+public class MyBlogApplication implements CommandLineRunner {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
@@ -22,4 +24,14 @@ public class MyBlogApplication  {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Override
+    public void run(String... args) throws Exception {
+        Role roleAdmin = new Role();
+        roleAdmin.setName("ROLE_ADMIN");
+        roleRepository.save(roleAdmin);
+
+        Role roleUser = new Role();
+        roleUser.setName("ROLE_USER");
+        roleRepository.save(roleUser);
+    }
 }
